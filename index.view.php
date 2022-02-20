4ef275cad2b349dd391f704fc275356c1a1221bf
jorgeorlandogonzalezguzman <jorge.o.gonzalez.dev@gmail.com>
2022-02-19 20:10:32 -0600

    init develop

--- index.view.php
+++ index.view.php
@@ -1,10 +1,22 @@
 <!DOCTYPE html>
 <html>
-    <hea>
+    <head>
         <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="with=device-with, initial-scale=1.0">
         <title>Nombre website</title>
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+
     </head>
     <body>
-
+        <div class="container">
+            <header class="row">
+            </header>
+            <div class="row">
+            </div>
+            <footer clas="row">
+            </footer>    
+        </div>
+        <scrip src="js/bootstrap.bundle.min.js">
     </body>
 </html>
